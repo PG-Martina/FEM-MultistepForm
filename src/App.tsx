3276d60f4428776a6250318b15d9fe3d71c0cbbb
Martina Peic Gavran <m.peicgavran@xximo.com>
2025-07-10 10:59:03 +0200

    deploy setup

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,19 @@ import Step3 from "./pages/Step3";
 import Step4 from "./pages/Step4";
 
 function App() {
-  const router = createHashRouter(
-    [
-      {
-        path: "/",
-        element: <Layout />,
-        children: [
-          { index: true, element: <Navigate to={Paths.Step1} replace /> },
-          { path: Paths.Step1, element: <Step1 /> },
-          { path: Paths.Step2, element: <Step2 /> },
-          { path: Paths.Step3, element: <Step3 /> },
-          { path: Paths.Step4, element: <Step4 /> },
-        ],
-      },
-    ],
+  const router = createHashRouter([
     {
-      basename: "/FEM-MultistepForm/",
-    }
-  );
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Navigate to={Paths.Step1} replace /> },
+        { path: Paths.Step1, element: <Step1 /> },
+        { path: Paths.Step2, element: <Step2 /> },
+        { path: Paths.Step3, element: <Step3 /> },
+        { path: Paths.Step4, element: <Step4 /> },
+      ],
+    },
+  ]);
   return <RouterProvider router={router} />;
 }
 
